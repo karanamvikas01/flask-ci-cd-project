46f7f02132408334a2bb219aee3283900296a13c
Karanam Vikas <karanamvikas01@gmail.com>
2026-05-12 16:38:10 +0530

    Final automated deployment pipeline

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Flask CI/CD Pipeline Running Successfully!"
+    return "AWS CI/CD Deployment Successful!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
